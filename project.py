e998519c9c1c44afa16afe27c694cef80f7df6b0
jimjshields <jim.j.shields@gmail.com>
2015-03-17 10:59:06 -0400

    Refactored BP class. Cleaned up unnecessary comments.

--- project.py
+++ project.py
@@ -14,78 +14,23 @@ class PageData(object):
 		   of the text."""
 
 		self.text = self.get_url_text(url)
-		self.text_soup = self.convert_html_to_bsoup(self.text)
 	
 	def get_url_text(self, url):
 		"""Given a url, return the text found at that url. Assumes the encoding
 		   of the response is the encoding indicated in the HTTP headers."""
-
-		# bytestr url => unicode text
 
 		url_response = requests.get(url)
 		url_text = url_response.text
 
 		return url_text
 
-	def convert_html_to_bsoup(self, url_text):
+	def get_soup_from_text(self):
 		"""Given url text, gets the text at that url and attempts to convert it to
 		   a BeautifulSoup object. Assumes that the text is HTML."""
 
-		# unicode text => BeautifulSoup object
-
-		text_soup = BeautifulSoup(url_text)
+		text_soup = BeautifulSoup(self.text)
 
 		return text_soup
-
-class BestPicturePageData(PageData):
-	"""Stores attributes and methods of getting data from the Best Picture Oscar Wiki page."""
-
-	def __init__(self):
-		"""Initializes with the BeautifulSoup object of the Wiki page."""
-
-		self.page_data = PageData('http://en.wikipedia.org/wiki/Academy_Award_for_Best_Picture')
-		self.text_soup = self.page_data.text_soup
-
-	def convert_li_to_movie_data(self, li):
-		"""Given a BeautifulSoup li object of a prespecified pattern, returns a list
-		   of movie_url, movie_title, and movie_year."""
-
-		movie_url = li.a['href']
-		movie_title = li.a.string
-
-		# Assumes that the year will always be found in parentheses
-		# following a </i> tag and prior to a </li> tag.
-
-		year_pattern = re.compile(r'</i>\s+\((.+)\)</li>')
-		movie_year = re.search(year_pattern, unicode(li)).group(1)
-		return [movie_url, movie_title, movie_year]
-
-	def get_bp_movie_data(self):
-		"""Specific to the Academy Award Best Picture Wiki page. Assumes that the
-		   list of BP winners will always be found in the same table."""
-
-		# unicode text => array of unicode text
-
-		tables = self.text_soup('table')
-
-		# By using negative indexing, assumes that it is more likely that other 
-		# tables will be added before this table, rather than after it. 
-		possible_winners = tables[-2]('li')
-
-		# Assumes that the movie names will always have the year in parentheses
-		# following them and uses this pattern to match the list item for each movie.
-		winner_pattern = re.compile(r'.+\(.+\)')
-		winners = (li for li in possible_winners 
-					if re.match(winner_pattern, unicode(li)))
-
-		movies = []
-
-		# Assumes that movies will continue to be structured as list items,
-		# with the url, title, and year found in the same place.
-		for li in winners:
-			movie_data = self.convert_li_to_movie_data(li)
-			movies.append(movie_data)
-		return movies
 
 class MovieData(PageData):
 	"""Stores attributes and methods of getting data from a movie's Wiki page."""
@@ -95,7 +40,7 @@ class MovieData(PageData):
 
 		self.movie_url = movie_url
 		self.page_data = PageData('http://en.wikipedia.org{0}'.format(self.movie_url))
-		self.text_soup = self.page_data.text_soup
+		self.text_soup = self.page_data.get_soup_from_text()
 		self.budget = self.get_movie_budget()
 		self.split_budget = self.split_budget_text(self.budget)
 		self.budget_int = self.convert_budget_to_int(self.split_budget)
@@ -103,8 +48,6 @@ class MovieData(PageData):
 	def get_movie_budget(self):
 		"""Given the url for a particular movie, returns the budget of the movie
 		   as a string, or as 'N/A' if not found."""
-
-		# string url => unicode budget
 		
 		# Assumes that the movie info will always be found in the first table.
 		# Given that it is the first table, I'm comfortable with the assumption
@@ -186,29 +129,71 @@ class MovieData(PageData):
 				# (trailing period).
 				return float(digits)
 
-def get_all_movie_budgets():
-	"""Gets all of the Best Picture movie data, and for each movie gets and
-	   appends its budget string and budget int to the table, returning the 
-	   table."""
+class BestPicturePageData(PageData):
+	"""Stores attributes and methods of getting data from the Best Picture Oscar Wiki page."""
 
-	movie_data = get_bp_movie_data()
-	for movie in movie_data:
-		movie.append(get_movie_budget(movie[0]))
-		movie.append(convert_budget_to_int(split_budget_text(movie[3])))
+	def __init__(self):
+		"""Initializes with the BeautifulSoup object of the Wiki page."""
 
-	return movie_data
+		self.page_data = PageData('http://en.wikipedia.org/wiki/Academy_Award_for_Best_Picture')
+		self.text_soup = self.page_data.get_soup_from_text()
 
-def get_average_budget():
-	"""After retrieving all of the movie data w/ budgets, returns the average
-	   budget of movies that provide a budget."""
+	def convert_li_to_movie_data(self, li):
+		"""Given a BeautifulSoup li object of a prespecified pattern, returns a tuple
+		   of movie_url, movie_title, and movie_year."""
 
-	movie_data = get_all_movie_budgets()
-	movies_with_budgets = [movie for movie in movie_data if movie[4] != 'N/A']
-	movie_budgets = [movie[4] for movie in movies_with_budgets]
-	budgets_sum = sum(movie_budgets)
-	average_budget = budgets_sum / len(movie_budgets)
+		movie_url = li.a['href']
+		movie_title = li.a.string
 
-	return '{:0,.2f}'.format(average_budget)
+		# Assumes that the year will always be found in parentheses
+		# following a </i> tag and prior to a </li> tag.
+
+		year_pattern = re.compile(r'</i>\s+\((.+)\)</li>')
+		movie_year = re.search(year_pattern, unicode(li)).group(1)
+		return movie_url, movie_title, movie_year
+
+	def get_bp_movie_data(self):
+		"""Given the data for the Best Picture Wiki page, builds an array of arrays of the
+		   movie urls, titles, years, and budgets."""
+
+		tables = self.text_soup('table')
+
+		# By using negative indexing, assumes that it is more likely that other 
+		# tables will be added before this table, rather than after it. 
+		possible_winners = tables[-2]('li')
+
+		# Assumes that the movie names will always have the year in parentheses
+		# following them and uses this pattern to match the list item for each movie.
+		winner_pattern = re.compile(r'.+\(.+\)')
+		winners = (li for li in possible_winners 
+					if re.match(winner_pattern, unicode(li)))
+
+		movies = []
+
+		# Assumes that movies will continue to be structured as list items,
+		# with the url, title, and year found in the same place.
+		for li in winners:
+			movie_url, movie_title, movie_year = self.convert_li_to_movie_data(li)
+			movie_budget_int = MovieData(movie_url).budget_int
+			movie_data = [movie_url, movie_title, movie_year, movie_budget_int]
+			print movie_data
+			movies.append(movie_data)
+		return movies
+
+	def get_average_budget(self):
+		"""After retrieving all of the movie data w/ budgets, returns the average
+		   budget of movies that provide a budget."""
+
+		self.bp_movie_data = self.get_bp_movie_data()
+		movies_with_budgets = [movie for movie in self.bp_movie_data if movie[3] != 'N/A']
+		movie_budgets = [movie[3] for movie in movies_with_budgets]
+		budgets_sum = sum(movie_budgets)
+		average_budget = budgets_sum / len(movie_budgets)
+
+		return '{:0,.2f}'.format(average_budget)
+
+bp_data = BestPicturePageData()
+print bp_data.get_average_budget()
 
 """If you encounter any edge cases, feel free to use your best judgement 
 and add a comment with your conclusion. This code should be written to 
